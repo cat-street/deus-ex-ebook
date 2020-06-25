63e22b7021902c0b07ee29f608d1a5e0298e61f7
Andrey Kudryavtsev <lomastering@gmail.com>
2020-06-25 21:09:02 +0300

    refactor: optimize js

--- deusexpad.js
+++ deusexpad.js
@@ -4,21 +4,21 @@ const articleImage = document.querySelector('.article__image');
 const lightbox = document.querySelector('.lightbox');
 
 const switchPage = page => {
-  for (let i = 0; i < navButton.length; i++) {
+  navButton.forEach((el, i) => {
     if (i === page) {
-      navButton[i].textContent = '●';
-      navButton[i].style.pointerEvents = 'none';
+      el.textContent = '●';
+      el.style.pointerEvents = 'none';
       article[i].hidden = false;
       article[i].style.opacity = 0;
       window.setTimeout(function () { // for CSS transition
         article[i].style.opacity = 1;
       }, 200);
     } else {
-      navButton[i].textContent = '○';
-      navButton[i].style.pointerEvents = 'auto';
+      el.textContent = '○';
+      el.style.pointerEvents = 'auto';
       article[i].hidden = true;
     }
-  }
+  });
 }
 
 const toggleLightbox = () => lightbox.classList.toggle('lightbox_hidden');
